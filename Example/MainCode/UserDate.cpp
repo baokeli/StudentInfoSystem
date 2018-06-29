c2231d441589168425289cf24f458420693a1333
baokeli <baokeli@bianfeng.com>
2018-06-29 17:54:51 +0800

    创建学生信息系统模板

---
+++ Example/MainCode/UserDate.cpp
@@ -0,0 +1,11 @@
+#include "UserDate.h"
+
+
+CUserDate::CUserDate()
+{
+}
+
+
+CUserDate::~CUserDate()
+{
+}
---
+++ Example/MainCode/UserDate.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+// 用户数据
+class CUserDate
+{
+public:
+	CUserDate();
+	~CUserDate();
+
+
+};
+
